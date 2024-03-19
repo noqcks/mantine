e929d0f7efabeb590e82a021ecb0c4c8070c9f75
Benji Visser <benji@093b.org>
2024-03-19 02:19:12 +0000

    Upgrade dependencies

--- packages/@docs/demos/src/demos/carousel/Carousel.demo.autoplay.tsx
+++ packages/@docs/demos/src/demos/carousel/Carousel.demo.autoplay.tsx
@@ -10,7 +10,7 @@ import Autoplay from 'embla-carousel-autoplay';
 import { Carousel } from '@mantine/carousel';
 
 function Demo() {
-  const autoplay = useRef(Autoplay({ delay: 2000 }));
+  const autoplay = useRef(Autoplay({ delay: 2000, jump: false, playOnInit: true, stopOnFocusIn: true, stopOnInteraction: true, stopOnMouseEnter: true, stopOnLastSnap: false, rootNode: null }));
   return (
     <Carousel
       withIndicators
